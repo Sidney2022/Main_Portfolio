51cf7373c3fd919bf62c8e4ce354f01fffada110
Sidney Uwaya <uwayasidney@gmail.com>
2022-08-26 09:19:00 +0100

    updated

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const switchModes = document.querySelector('.switch')
 const main = document.querySelector('main')
 const sideBar = document.querySelector('.sidebar')
 const mainContent = document.querySelector('.main_section')
+const navBar = document.querySelector('.navbar')
 
 switchModes.addEventListener('click', () => {
   switchModes.classList.toggle('active')
   main.classList.toggle('active')
   sideBar.classList.toggle('active')
   mainContent.classList.toggle('active')
+  navBar.classList.toggle('active')
 })
